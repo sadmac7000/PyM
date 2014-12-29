dc918d58b8aa6a375ca594c971b7a629ac7d00a4
Casey Dahlin <casey.dahlin@gmail.com>
2014-12-28 21:34:15 -0500

    Fix capitalization in license notices
    Signed-off-by: Casey Dahlin <casey.dahlin@gmail.com>

--- pym/urwid_ui.py
+++ pym/urwid_ui.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright © 2014, 2015 Casey Dahlin, John H. Dulaney
 #
-# this file is part of PyM.
+# This file is part of PyM.
 #
-# PyM is free software: you can redistribute it and/or modify it under the
-# terms of the gnu general public license as published by the free software
-# foundation, either version 3 of the license, or (at your option) any later
-# version.
+# PyM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# PyM is distributed in the hope that it will be useful, but without any
-# warranty; without even the implied warranty of merchantability or fitness for
-# a particular purpose.  see the gnu general public license for more details.
+# PyM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
-# you should have received a copy of the gnu general public license along with
-# PyM. if not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# PyM.  If not, see <http://www.gnu.org/licenses/>.
 
 import urwid
 from .ui_core import ui, UI
