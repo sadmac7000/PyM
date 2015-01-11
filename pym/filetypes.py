75f9267db045a2d00d74e2095c0c78869a27ece3
Casey Dahlin <casey.dahlin@gmail.com>
2015-01-10 23:29:51 -0500

    Add missing files
    Signed-off-by: Casey Dahlin <casey.dahlin@gmail.com>

---
+++ pym/filetypes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2014, 2015 Casey Dahlin, John H. Dulaney
+#
+# This file is part of PyM.
+#
+# PyM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# PyM is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PyM.  If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=too-few-public-methods
+
+class FileType(object):
+    pass
+
+MIME_DICT = {}
+
+plain_text = FileType()
+
+def file_type_for_mime(mime):
+    if mime in MIME_DICT:
+        return MIME_DICT[mime]
+
+    return plain_text
